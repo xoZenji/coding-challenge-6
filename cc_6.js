c7048a5f3a686f523acabdceb05977470321d7ea
Jordan Higgins <jordanhiggins@usf.edu>
2025-02-09 20:30:12 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -7,3 +7,13 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 console.log(`Total Profit: $${calculateProfit(20, 30, 100)}`);
 console.log(`Total Profit: $${calculateProfit(50, 70, 200)}`);
 // Logs the profits to the console.
+
+//Task 2: Sales Tax Computation
+function calculateSalesTax(amount, taxRate) {
+    let salesTax = amount * taxRate
+    return Math.floor(salesTax);
+}; // Calculates the sales tax
+
+console.log(`Sales Tax: $${calculateSalesTax(100, 0.07)}`);
+console.log(`Sales Tax: $${calculateSalesTax(500, 0.1)}`);
+// Logs the tax to the console.
